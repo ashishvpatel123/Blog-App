fd892a2dff9a37419a9c66aa5af0fb9466957c08
Ashish <ashishvpatel123@gmail.com>
2019-02-21 11:20:38 -0600

    add static tableview (sub details)

--- Blog App/ViewController.swift
+++ Blog App/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
 
 }
 
+
+extension ViewController : UITableViewDelegate,UITableViewDataSource   {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "blogCell")!
+        
+        cell.textLabel?.text = "Ashish"
+        cell.detailTextLabel?.text = "sub details"
+        
+        return cell
+    }
+    
+    
+}
